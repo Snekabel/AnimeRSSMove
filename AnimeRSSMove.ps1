938097e7a3706a5b4e830e1512a9a59d6163ade8
Charlie Skog <charlie.skog@gmail.com>
2019-10-11 13:58:16 +0200

    Update AnimeRSSMove.ps1
    Added compatibility with more clients by making a "InputPath" feature, also added trimming for "OutputPath".

--- AnimeRSSMove.ps1
+++ AnimeRSSMove.ps1
@@ -1,49 +1,102 @@
-#=============================================================================
-# AnimeRSSMove v1.0 by Neo
-# AnimeRSSMove.ps1 -InputFile '<file path>' -OutputPath = '<file destination>'
-#=============================================================================
-### Parameters, Title, Checks, Variables
+#================================================================================
+# AnimeRSSMove v2.0 by Neo
+# AnimeRSSMove.ps1 -OutputPath = '<file destination>' -InputFile '<file path>'
+# AnimeRSSMove.ps1 -OutputPath = '<files destination>' -InputPath '<folder path>'
+#================================================================================
+### Parameters, Title, Checks, Variables, Script
 # Set parameters
 Param(
 	[Parameter(Mandatory=$true, Position=0)]
+	[string]$OutputPath = "",
+	[Parameter(Mandatory=$false, Position=1)]
 	[string]$InputFile = "",
-	[Parameter(Mandatory=$true, Position=1)]
-	[string]$OutputPath = ""
+	[Parameter(Mandatory=$false, Position=2)]
+	[string]$InputPath = ""
 )
 
 # Set PowerShell title.
-$host.ui.RawUI.WindowTitle = "AnimeRSSMove v1.0 by Neo"
+$host.ui.RawUI.WindowTitle = "AnimeRSSMove v2.0 by Neo"
 
 # Wait 5 seconds to bypass errors created by other software.
 Start-Sleep -Seconds 5
 
-# Check if input file exists, if it doesn't exit script.
-If (-Not ([System.IO.File]::Exists($InputFile))) {
-	exit
-}
-
-# Check if output path exists, if it doesn't exit script.
-If (-Not (Test-Path $OutputPath)) {
-	exit
-}
-
 # Default variables.
-$InputFileName = Split-Path $InputFile -Leaf
-$InputFileStartSubstring = $InputFileName.IndexOf("]")
-$InputFileEndSubstring = $InputFileName.LastIndexOf("-")
-$FolderName = $InputFileName.Substring(0, $InputFileEndSubstring).Substring($InputFileStartSubstring+1).Trim()
-$FolderPath = $OutputPath + $FolderName
-$FolderPathFileName = $FolderPath + "\" + $InputFileName
+$TrueOutputPath = $OutputPath.TrimEnd('\')
 
-# Check if folder path exists, if it doesn't create folder.
-If (-Not (Test-Path $FolderPath)) {
-	New-Item -Path $OutputPath -Name $FolderName -ItemType "directory"
+# Script if InputFile exists.
+If ($InputFile) {
+	# Check if input file exists, if it doesn't exit script.
+	If (-Not ([System.IO.File]::Exists($InputFile))) {
+		Write-Host "Input file doesn't exist."
+		break
+	}
+
+	# Check if output path exists, if it doesn't exit script.
+	If (-Not (Test-Path $TrueOutputPath)) {
+		Write-Host "Output path doesn't exist."
+		break
+	}
+
+	# Script variables.
+	$InputFileName = Split-Path $InputFile -Leaf
+	$InputFileStartSubstring = $InputFileName.IndexOf("]")
+	$InputFileEndSubstring = $InputFileName.LastIndexOf("-")
+	$FolderName = $InputFileName.Substring(0, $InputFileEndSubstring).Substring($InputFileStartSubstring+1).Trim()
+	$FolderPath = $TrueOutputPath + "\" + $FolderName
+	$FolderPathFileName = $FolderPath + "\" + $InputFileName
+
+	# Check if folder path exists, if it doesn't create folder.
+	If (-Not (Test-Path $FolderPath)) {
+		New-Item -Path $TrueOutputPath -Name $FolderName -ItemType "directory"
+	}
+
+	# Check if file already exists in the path, if it does delete it.
+	If ([System.IO.File]::Exists($FolderPathFileName)) {
+		[System.IO.File]::Delete($FolderPathFileName)
+	}
+
+	# Move file to new folder.
+	[System.IO.File]::Move($Inputfile, $FolderPathFileName)
 }
 
-# Check if file already exists in the path, if it does delete it.
-If ([System.IO.File]::Exists($FolderPathFileName)) {
-	[System.IO.File]::Delete($FolderPathFileName)
-}
+# Script if InputPath exists.
+If ($InputPath) {
+	# Get files from InputPath.
+	$InputPathFiles = Get-ChildItem -File -Path $InputPath -Recurse
+	
+	# Run ForEach loop on all files in InputPath.
+	ForEach ($Item in $InputPathFiles) {
+		# Check if input file exists, if it doesn't exit script.
+		If (-Not ([System.IO.File]::Exists($Item.FullName))) {
+			Write-Host $Item "in input path array doesn't exist."
+			break
+		}
 
-# Move file to new folder.
-[System.IO.File]::Move($Inputfile, $FolderPathFileName)
+		# Check if output path exists, if it doesn't exit script.
+		If (-Not (Test-Path $TrueOutputPath)) {
+			Write-Host "Output path doesn't exist."
+			break
+		}
+
+		# Script variables.
+		$InputFileName = Split-Path $Item -Leaf
+		$InputFileStartSubstring = $InputFileName.IndexOf("]")
+		$InputFileEndSubstring = $InputFileName.LastIndexOf("-")
+		$FolderName = $InputFileName.Substring(0, $InputFileEndSubstring).Substring($InputFileStartSubstring+1).Trim()
+		$FolderPath = $TrueOutputPath + "\" + $FolderName
+		$FolderPathFileName = $FolderPath + "\" + $InputFileName
+
+		# Check if folder path exists, if it doesn't create folder.
+		If (-Not (Test-Path $FolderPath)) {
+			New-Item -Path $TrueOutputPath -Name $FolderName -ItemType "directory"
+		}
+
+		# Check if file already exists in the path, if it does delete it.
+		If ([System.IO.File]::Exists($FolderPathFileName)) {
+			[System.IO.File]::Delete($FolderPathFileName)
+		}
+
+		# Move file to new folder.
+		[System.IO.File]::Move($Item.FullName, $FolderPathFileName)
+	}
+}
