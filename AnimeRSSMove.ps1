15593628aa8515cb5a2d0181c9ffbdb405c1ef32
NeoTheOne <charlie.skog@gmail.com>
2022-12-07 00:34:47 +0100

    Update AnimeRSSMove.ps1

--- AnimeRSSMove.ps1
+++ AnimeRSSMove.ps1
@@ -1,5 +1,5 @@
 #================================================================================
-# AnimeRSSMove v4.0 by Neo
+# AnimeRSSMove v5.0 by Neo
 # AnimeRSSMove.ps1 -OutputPath "<file destination>" -InputFile "<file path>"
 # AnimeRSSMove.ps1 -OutputPath "<files destination>" -InputPath "<folder path>"
 #================================================================================
@@ -15,63 +15,63 @@ Param(
 )
 
 # Set PowerShell title.
-$host.ui.RawUI.WindowTitle = "AnimeRSSMove v4.0 by Neo"
+$host.ui.RawUI.WindowTitle = "AnimeRSSMove v5.0 by Neo"
 
 # Wait 5 seconds to bypass errors created by other software.
 # Start-Sleep -Seconds 5
 
-# Default variables.
+# Define default variables.
 $OutputPath = $OutputPath.TrimStart("'").TrimEnd("'")
 $InputFile = $InputFile.TrimStart("'").TrimEnd("'")
 $InputPath = $InputPath.TrimStart("'").TrimEnd("'")
 $TrueOutputPath = $OutputPath.TrimEnd("\")
 
-# Script if InputFile exists.
+# Define error codes and messages variables.
+$ErrorInputFileDoesNotExist = "Error code 1: Input file doesn't exist."
+$ErrorInputPathDoesNotExist = "Error code 2: Input path doesn't exist."
+$ErrorOutputPathDoesNotExist = "Error code 3: Output path doesn't exist."
+$ErrorMovingFile = "Error code 4: Error moving file."
+
+# Define log file path variable.
+$LogFilePath = "C:\temp\AnimeRSSMove.log"
+
+# Get current date and time.
+$DateTime = Get-Date
+
+# Script if InputFile variable exists.
 If ($InputFile) {
-	# Check if input file exists, if it doesn't exit script.
+	# Check if input file exists, and write error message to log and exit script if it doesn't.
 	If (-Not ([System.IO.File]::Exists($InputFile))) {
-		Write-Host "Input file doesn't exist."
-		Write-Host $InputFile
-		break
+		# Write error message to log file.
+		Add-Content -Path $LogFilePath -Value "$DateTime - $ErrorInputFileDoesNotExist - $InputFile"
+
+		# Write error message to console and exit script.
+		Write-Error "$DateTime - $ErrorInputFileDoesNotExist - $InputFile" -Action Stop
 	}
 
-	# Check if output path exists, if it doesn't exit script.
+	# Check if output path exists, and write error message to log and exit script if it doesn't.
 	If (-Not (Test-Path $TrueOutputPath)) {
-		Write-Host "Output path doesn't exist."
-		Write-Host $OutputPath
-		break
+		# Write error message to log file.
+		Add-Content -Path $LogFilePath -Value "$DateTime - $ErrorOutputPathDoesNotExist - $OutputPath"
+
+		# Write error message to console and exit script.
+		Write-Error "$DateTime - $ErrorOutputPathDoesNotExist - $OutputPath" -Action Stop
 	}
 
 	# Script variables.
 	$InputFileName = Split-Path $InputFile -Leaf
 	$InputFileStartSubstring = $InputFileName.IndexOf("]")
 	$InputFileEndSubstring = $InputFileName.LastIndexOf(" - ")
-	$FolderName = $InputFileName.Substring(0, $InputFileEndSubstring).Substring($InputFileStartSubstring+1).Trim()
+	# Welcome to REGEX HELL.
+	$FolderName = $InputFileName -replace "\d+(\.[a-zA-Z]{3,4})$", "" -replace '(\d+)(\s\[TV\sx264\s10bit\s1080p\sAAC\s2.0\sDual\]).*', '' -replace '( - \d{1,2}(S\d{1,2}E\d{1,2})?$).*', '' -replace '(\d+)(\s\(BD\s1080p\)\.[mkv|avi|mp4]+)', '' -replace '(_\d+v?\d*).*', '' -replace '_', ' ' -replace " - SP\d+", "" -replace '\[.*?\]', '' -replace '\.mkv|\.avi|\.mp4', '' -replace "\.", " " -replace '\(.*?\)', '' -replace '( - \d{1,2}(S\d{1,2}E\d{1,2})?$).*', '' -replace '( - S\d+E\d+| - \d+).*', '' -replace '(S\d+E\d+).*', '' -replace '(.(SP|EP)\d+)*', '' -replace '(e\d+.*)', '' -replace '(\sep\s*\d+).*', '' -replace '( -$)', '' -replace '( - $)', '' -replace '- S\d+E?\b', ''
+	$FolderName = $FolderName.Trim()
 	$FolderPath = $TrueOutputPath + "\" + $FolderName
 	$FolderPathFileName = $FolderPath + "\" + $InputFileName
 
-	# Replace "X" with "Y" to circumvent any problems in Plex.
-	$FolderPath = $FolderPath.replace("S2", "Season 2")
-	$FolderName = $FolderName.replace("S2", "Season 2")
-	$FolderPathFileName = $FolderPathFileName.replace("S2", "Season 2")
-	$FolderPath = $FolderPath.replace("S3", "Season 3")
-	$FolderName = $FolderName.replace("S3", "Season 3")
-	$FolderPathFileName = $FolderPathFileName.replace("S3", "Season 3")
-	$FolderPath = $FolderPath.replace("S4", "Season 4")
-	$FolderName = $FolderName.replace("S4", "Season 4")
-	$FolderPathFileName = $FolderPathFileName.replace("S4", "Season 4")
-	$FolderPath = $FolderPath.replace("S5", "Season 5")
-	$FolderName = $FolderName.replace("S5", "Season 5")
-	$FolderPathFileName = $FolderPathFileName.replace("S5", "Season 5")
-	$FolderPath = $FolderPath.replace("S6", "Season 6")
-	$FolderName = $FolderName.replace("S6", "Season 6")
-	$FolderPathFileName = $FolderPathFileName.replace("S6", "Season 6")
-	$FolderPath = $FolderPath.replace("S7", "Season 7")
-	$FolderName = $FolderName.replace("S7", "Season 7")
-	$FolderPathFileName = $FolderPathFileName.replace("S7", "Season 7")
-	$FolderPath = $FolderPath.replace("S8", "Season 8")
-	$FolderName = $FolderName.replace("S8", "Season 8")
-	$FolderPathFileName = $FolderPathFileName.replace("S8", "Season 8")
+	# Replace "SX -" with "Season X -" and remove any 0's in front to circumvent any problems in Plex.
+	$FolderName = $FolderName -replace 'S0*(\d+)$', 'Season $1'
+	$FolderPath = $FolderPath -replace 'S0*(\d+)$', 'Season $1'
+	$FolderPathFileName = $FolderPathFileName -replace 'S0*(\d+)$', 'Season $1'
 
 	# Check if folder path exists, if it doesn't create folder.
 	If (-Not (Test-Path $FolderPath)) {
@@ -83,12 +83,36 @@ If ($InputFile) {
 		[System.IO.File]::Delete($FolderPathFileName)
 	}
 
-	# Move file to new folder.
-	[System.IO.File]::Move($Inputfile, $FolderPathFileName)
+	# Move input file to output folder and check if move was sucessfully ran.
+	if (-Not [System.IO.File]::Move($InputFile, $FolderPathFileName)) {
+		# Write error message to log file.
+		Add-Content -Path $LogFilePath -Value "$DateTime - $ErrorMovingFile - $InputFile to $FolderPathFileName"
+
+		# Write error message to console.
+		Write-Error "$DateTime - $ErrorMovingFile - $InputFile to $FolderPathFileName" -Action Stop
+	}
 }
 
-# Script if InputPath exists.
+# Script if InputPath variable exists.
 If ($InputPath) {
+	# Check if input path exists, and write error message to log and exit script if it doesn't.
+	If (-Not (Test-Path $InputPath)) {
+		# Write error message to log file.
+		Add-Content -Path $LogFilePath -Value "$DateTime - $ErrorInputPathDoesNotExist - $InputPath"
+
+		# Write error message to console and exit script.
+		Write-Error "$DateTime - $ErrorInputPathDoesNotExist - $InputPath" -Action Stop
+	}
+
+	# Check if output path exists, and write error message to log and exit script if it doesn't.
+	If (-Not (Test-Path $TrueOutputPath)) {
+		# Write error message to log file.
+		Add-Content -Path $LogFilePath -Value "$DateTime - $ErrorOutputPathDoesNotExist - $OutputPath"
+
+		# Write error message to console and exit script.
+		Write-Error "$DateTime - $ErrorOutputPathDoesNotExist - $OutputPath" -Action Stop
+	}
+
 	# Get files from InputPath.
 	$InputPathFiles = Get-ChildItem -File -Path $InputPath -Recurse
 	
@@ -100,19 +124,20 @@ If ($InputPath) {
 			break
 		}
 
-		# Check if output path exists, if it doesn't exit script.
-		If (-Not (Test-Path $TrueOutputPath)) {
-			Write-Host "Output path doesn't exist."
-			break
-		}
-
 		# Script variables.
 		$InputFileName = Split-Path $Item -Leaf
 		$InputFileStartSubstring = $InputFileName.IndexOf("]")
 		$InputFileEndSubstring = $InputFileName.LastIndexOf(" - ")
-		$FolderName = $InputFileName.Substring(0, $InputFileEndSubstring).Substring($InputFileStartSubstring+1).Trim()
+		# Welcome to REGEX HELL.
+		$FolderName = $InputFileName -replace "\d+(\.[a-zA-Z]{3,4})$", "" -replace '(\d+)(\s\[TV\sx264\s10bit\s1080p\sAAC\s2.0\sDual\]).*', '' -replace '( - \d{1,2}(S\d{1,2}E\d{1,2})?$).*', '' -replace '(\d+)(\s\(BD\s1080p\)\.[mkv|avi|mp4]+)', '' -replace '(_\d+v?\d*).*', '' -replace '_', ' ' -replace " - SP\d+", "" -replace '\[.*?\]', '' -replace '\.mkv|\.avi|\.mp4', '' -replace "\.", " " -replace '\(.*?\)', '' -replace '( - \d{1,2}(S\d{1,2}E\d{1,2})?$).*', '' -replace '( - S\d+E\d+| - \d+).*', '' -replace '(S\d+E\d+).*', '' -replace '(.(SP|EP)\d+)*', '' -replace '(e\d+.*)', '' -replace '(\sep\s*\d+).*', '' -replace '( -$)', '' -replace '( - $)', '' -replace '- S\d+E?\b', ''
+		$FolderName = $FolderName.Trim()
 		$FolderPath = $TrueOutputPath + "\" + $FolderName
 		$FolderPathFileName = $FolderPath + "\" + $InputFileName
+		
+		# Replace "SX -" with "Season X -" and remove any 0's in front to circumvent any problems in Plex.
+		$FolderName = $FolderName -replace 'S0*(\d+)$', 'Season $1'
+		$FolderPath = $FolderPath -replace 'S0*(\d+)$', 'Season $1'
+		$FolderPathFileName = $FolderPathFileName -replace 'S0*(\d+)$', 'Season $1'
 
 		# Check if folder path exists, if it doesn't create folder.
 		If (-Not (Test-Path $FolderPath)) {
@@ -124,7 +149,13 @@ If ($InputPath) {
 			[System.IO.File]::Delete($FolderPathFileName)
 		}
 
-		# Move file to new folder.
-		[System.IO.File]::Move($Item.FullName, $FolderPathFileName)
+		# Move input file to output folder and check if move was sucessfully ran.
+		if (-Not [System.IO.File]::Move($InputFile, $FolderPathFileName)) {
+			# Write error message to log file.
+			Add-Content -Path $LogFilePath -Value "$DateTime - $ErrorMovingFile - $InputFile to $FolderPathFileName"
+
+			# Write error message to console.
+			Write-Error "$DateTime - $ErrorMovingFile - $InputFile to $FolderPathFileName" -Action Stop
+		}
 	}
 }
